04d1a031ca5eb0224d215b0ce03d245ac9103e46
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-07-09 20:05:25 +0300

    0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './css/styles.css';
 
+const input = document.querySelector('#search-box');
+const countryCard = document.querySelector('.country-info');
+
 const DEBOUNCE_DELAY = 300;
+
+const countriesUrl =
+  'https://restcountries.com/v2/name/peru?fields=name.official,capital,population,flags.svg,languages';
+
+input.addEventListener('input', onInput);
+
+function onInput(e) {
+  const countryEl = e.currentTarget.value;
+}
+
+fetch(countriesUrl)
+  .then(responsive => responsive.json())
+  .then(country => {
+    console.log(country);
+  })
+  .catch(error => {
+    console.log(error);
+  });
